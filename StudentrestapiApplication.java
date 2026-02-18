f987f30588ff3a3789a434f92d915bdddd6f8cd7
saralayerra <144708522+saralayerra@users.noreply.github.com>
2026-02-18 11:34:10 +0530

    Add files via upload

---
+++ StudentrestapiApplication.java
@@ -0,0 +1,13 @@
+package om.springboot.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentrestapiApplication.class, args);
+	}
+
+}
